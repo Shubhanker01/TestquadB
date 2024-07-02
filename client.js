6f0892c22bd609cf7a62d578fb5cd1e0f66f50fc
Shubhanker01 <shubhanker40@gmail.com>
2024-07-02 18:31:33 +0530

    commit4

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 let classrow1 = document.getElementsByClassName('td1')
 let classrow2 = document.getElementsByClassName('td2')
 let classrow3 = document.getElementsByClassName('td3')
+let classrow4 = document.getElementsByClassName('td4')
+let classrow5 = document.getElementsByClassName('td5')
 let resArr = []
+
 async function fetchData() {
     let headersList = {
         "Accept": "*/*"
@@ -22,6 +25,12 @@ fetchData().then(function (res) {
         classrow1[i].innerText = resArr[i].name
         classrow2[i].innerText = resArr[i].last_traded_price
         classrow3[i].innerText = `₹${resArr[i].buy} / ₹${resArr[i].sell}`
+        let diff = ((resArr[i].sell - resArr[i].buy)/resArr[i].buy)*100
+        if(resArr[i].buy==0.0){
+            diff=0.0
+        }
+        classrow4[i].innerText = `${Math.round(diff*100)/100}%`
+        classrow5[i].innerText = `₹${Math.round(resArr[i].vol*100)/100}`
     }
 })
 console.log(classrow1)
